94d7bff05180761244ad90e7d48bef44387b5d34
Brian Tran <exobrian@yahoo.com>
2022-07-31 16:18:48 -0700

    Initial Commit

---
+++ Lyrics.py
@@ -0,0 +1,10 @@
+# Make HTTP requests
+import requests
+# Scrape data from an HTML document
+from bs4 import BeautifulSoup
+# I/O
+import os
+# Search and manipulate strings
+import re
+
+print("Hello!")
